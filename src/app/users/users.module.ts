26538785d42d1422217fd41115497ed7ff122144
Ivaylo Ivanov <funcrow1349@gmail.com>
2024-03-18 15:43:57 +0200

    Update

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileDetailsComponent } from './profile-details/profile-details.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
+import { DeleteProfileComponent } from './delete-profile/delete-profile.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    ProfileDetailsComponent,
+    EditProfileComponent,
+    DeleteProfileComponent
+  ],
   imports: [
     CommonModule
   ]
