9ca9c0c419450a01b47c7a309dc185750e9763ae
Ivaylo Ivanov <funcrow1349@gmail.com>
2024-03-24 14:33:22 +0200

    Update

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,9 +1,72 @@
 import { Injectable } from '@angular/core';
+import { UserForAuth } from '../types/user';
+import { BehaviorSubject, Subscription, tap } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private user$$ = new BehaviorSubject<UserForAuth | undefined>(undefined);
+  private user$ = this.user$$.asObservable();
 
-  constructor() { }
+  user: UserForAuth | undefined
+  USER_KEY = "[user]"
+
+  userSubscription: Subscription;
+
+  constructor(private http: HttpClient) {
+    this.userSubscription = this.user$.subscribe((user) => {
+      this.user = user;
+    })
+  }
+
+  register(
+    username: string,
+    email: string,
+    tel: string,
+    password: string,
+    rePassword: string
+  ) {
+    return this.http
+      .post<UserForAuth>('/api/register', {
+        username,
+        email,
+        tel,
+        password,
+        rePassword
+      })
+      .pipe(tap((user) => this.user$$.next(user)))
+  }
+
+  getProfile() {
+    return this.http
+      .get<UserForAuth>('/api/users/profile')
+      .pipe(tap((user) => this.user$$.next(user)))
+  }
+
+  updateProfile(username: string, email: string, tel?: string) {
+    return this.http
+      .put<UserForAuth>('/api/users/profile', {
+        username, email, tel,
+      })
+      .pipe(tap((user) => this.user$$.next(user)))
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<UserForAuth>('/api/login', {email, password})
+      .pipe(tap((user) => this.user$$.next(user)));
+  }
+
+  logout() {
+    return this.http
+      .post('/api/logout', {})
+      .pipe(tap(() => this.user$$.next(undefined)))
+  }
+
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe()
+  }
+
 }
