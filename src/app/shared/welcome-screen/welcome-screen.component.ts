a175b5a8f49d77dc074e29197215fd42e2da1333
Ivaylo Ivanov <funcrow1349@gmail.com>
2024-03-26 16:11:01 +0200

    Update

--- src/app/shared/welcome-screen/welcome-screen.component.ts
+++ src/app/shared/welcome-screen/welcome-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/users/user.service';
 
 @Component({
   selector: 'app-welcome-screen',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./welcome-screen.component.css']
 })
 export class WelcomeScreenComponent {
+  constructor(private userService: UserService) {}
 
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
+  }
 }
