b5e00d22cd10ca4069acbeaa4325a2a63c61ee8a
Ivaylo Ivanov <funcrow1349@gmail.com>
2024-03-27 22:39:31 +0200

    Update

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ProfileDetails, UserForAuth } from '../types/user';
+import { UserForAuth } from '../types/user';
 import { BehaviorSubject, Subscription, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
