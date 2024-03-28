d6f9dd304eca8bc32c765c08d80e80f797b74c15
Ivaylo Ivanov <funcrow1349@gmail.com>
2024-03-28 15:27:51 +0200

    Update

--- src/app/themes/latest-activity/latest-activity.component.ts
+++ src/app/themes/latest-activity/latest-activity.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Theme } from 'src/app/types/theme';
+import { ThemesService } from '../themes.service';
 
 @Component({
   selector: 'app-latest-activity',
   templateUrl: './latest-activity.component.html',
   styleUrls: ['./latest-activity.component.css']
 })
-export class LatestActivityComponent {
+export class LatestActivityComponent implements OnInit{
+  themes: Theme[] = []
 
+  constructor (private themesService: ThemesService) {}
+
+  ngOnInit(): void {
+    this.loadThemes();
+  }
+
+  loadThemes(): void {
+    this.themesService.getThemes().subscribe(themes => {
+      this.themes = themes.sort((a, b) => {
+        return new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime();
+      }).slice(0,3);
+    });
+  }
 }
