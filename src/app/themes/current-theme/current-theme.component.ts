3fc216ab620645bf4edf7a063f3f6033e1cf1a9a
Ivaylo Ivanov <funcrow1349@gmail.com>
2024-03-29 13:46:13 +0200

    Update

--- src/app/themes/current-theme/current-theme.component.ts
+++ src/app/themes/current-theme/current-theme.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Theme } from 'src/app/types/theme';
+import { ThemesService } from '../themes.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-current-theme',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./current-theme.component.css']
 })
 export class CurrentThemeComponent {
+  theme = {} as Theme;
 
+  constructor(
+    private themesService: ThemesService,
+    private activeRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.activeRoute.params.subscribe((data) => {
+      const id = data['themeId'];
+
+      this.themesService.getTheme(id).subscribe((theme) => {
+        this.theme = theme;
+      });
+    });
+  }
 }
